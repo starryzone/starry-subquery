8b2795794d8214061d6392c24b5e74ef7c15dfcf
Mike Purvis <mikedotexe@gmail.com>
2022-06-26 11:59:10 -0700

    initial commit for The Strange Clan SubQuery

---
+++ README.md
@@ -0,0 +1,31 @@
+# SubQuery for The Strange Clan NFT
+
+This is based on the [starter project for Juno](https://github.com/subquery/juno-subql-starter).
+
+This is looking at events and assumes that [cw721 standard](https://github.com/CosmWasm/cw-nfts/tree/main/packages/cw721) is followed.
+
+# Getting Started
+
+### 1. Install dependencies
+
+```shell
+yarn
+```
+
+### 2. Generate types
+
+```shell
+yarn codegen
+```
+
+### 3. Build
+
+```shell
+yarn build
+```
+
+### 4. Run locally
+
+```shell
+yarn start:docker
+```
---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,37 @@
+import { ExecuteEvent } from "../types";
+import { CosmosEvent } from "@subql/types-cosmos";
+
+export async function handleEvent(event: CosmosEvent): Promise<void> {
+  const eventRecord = new ExecuteEvent(
+    `${event.tx.hash}-${event.msg.idx}-${event.idx}`
+  );
+  eventRecord.blockHeight = BigInt(event.block.block.header.height);
+  eventRecord.txHash = event.tx.hash;
+  for (const attr of event.event.attributes) {
+    switch (attr.key) {
+      case "_contract_address":
+        eventRecord.contractAddress = attr.value;
+        break;
+      case "action":
+        eventRecord.action = attr.value;
+        break;
+      case "sender":
+        eventRecord.sender = attr.value;
+        break;
+      case "recipient":
+        eventRecord.recipient = attr.value;
+        break;
+      case "spender":
+        eventRecord.spender = attr.value;
+        break;
+      case "tokenId":
+        eventRecord.tokenId = attr.value;
+        break;
+      case "operator":
+        eventRecord.operator = attr.value;
+        break;
+      default:
+    }
+  }
+  await eventRecord.save();
+}
